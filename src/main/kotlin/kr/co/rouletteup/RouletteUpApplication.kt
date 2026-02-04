45bd4e94254cbec470940df37ad9b69f75207ae4
DaeYoung0726 <pdy000726@naver.com>
2026-02-04 22:25:53 +0900

    chore: create project

---
+++ src/main/kotlin/kr/co/rouletteup/RouletteUpApplication.kt
@@ -0,0 +1,11 @@
+package kr.co.rouletteup
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RouletteUpApplication
+
+fun main(args: Array<String>) {
+    runApplication<RouletteUpApplication>(*args)
+}
