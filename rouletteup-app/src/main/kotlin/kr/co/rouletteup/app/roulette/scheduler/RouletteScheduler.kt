a8c52487b2e3060ba315174db73d4617ff9d116c
DaeYoung0726 <pdy000726@naver.com>
2026-02-05 18:29:36 +0900

    feat: 자정마다 룰렛 생성 스케줄링 등록

---
+++ rouletteup-app/src/main/kotlin/kr/co/rouletteup/app/roulette/scheduler/RouletteScheduler.kt
@@ -0,0 +1,19 @@
+package kr.co.rouletteup.app.roulette.scheduler
+
+import kr.co.rouletteup.app.roulette.service.RouletteCommandService
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+
+@Component
+class RouletteScheduler(
+    private val rouletteCommandService: RouletteCommandService,
+) {
+
+    /**
+     * 매일 자정 룰렛 생성 로직 실행
+     */
+    @Scheduled(cron = "*/10 * * * * *")
+    fun createDailyRoulette() {
+        rouletteCommandService.createTodayIfNotExists()
+    }
+}
