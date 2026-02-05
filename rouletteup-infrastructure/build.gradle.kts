768982ba80b2af0575b1b6c0371b3bf8eb0c1cb2
DaeYoung0726 <pdy000726@naver.com>
2026-02-05 17:03:04 +0900

    chore: Swagger 디펜던시 추가

--- rouletteup-infrastructure/build.gradle.kts
+++ rouletteup-infrastructure/build.gradle.kts
@@ -3,4 +3,7 @@ tasks.jar { enabled = true }
 
 dependencies {
     implementation(project(":rouletteup-common"))
+
+    /* Swagger */
+    api("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.13")
 }
