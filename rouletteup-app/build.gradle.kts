e65987839f38521bfbcc3cde16084382403db9a8
박대영 <125343432+daeyoung0726@users.noreply.github.com>
2026-02-05 14:22:56 +0900

    Chore: Infrastructure 모듈 추가 (#9)
    * chore: infrastructure 모듈 생성
    
    * chore: 프로젝트 정의에 infra 추가
    
    * chore: infra 모듈 의존성 추기
    
    * chore: 프로필 그룹에 infrastructure 추가
    
    * chore: cd 배포 결정 조건에 infra 모듈 추가

--- rouletteup-app/build.gradle.kts
+++ rouletteup-app/build.gradle.kts
@@ -4,6 +4,7 @@ tasks.jar { enabled = false }
 dependencies {
     implementation(project(":rouletteup-common"))
     implementation(project(":rouletteup-domain"))
+    implementation(project(":rouletteup-infrastructure"))
 
     /* Spring */
     implementation("org.springframework.boot:spring-boot-starter-validation")
