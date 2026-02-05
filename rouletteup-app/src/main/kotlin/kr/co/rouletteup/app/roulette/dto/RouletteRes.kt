e832082e48e9d5679ac23b28cba39e8b4939c36b
DaeYoung0726 <pdy000726@naver.com>
2026-02-06 01:07:19 +0900

    refact: 룰렛 예산 응답 정보 변경 * 잔여 예산 -> 총 예산, 사용 예산

--- rouletteup-app/src/main/kotlin/kr/co/rouletteup/app/roulette/dto/RouletteRes.kt
+++ rouletteup-app/src/main/kotlin/kr/co/rouletteup/app/roulette/dto/RouletteRes.kt
@@ -3,13 +3,15 @@ package kr.co.rouletteup.app.roulette.dto
 import kr.co.rouletteup.domain.roulette.entity.DailyRoulette
 
 data class RouletteRes(
-    val remainingBudget: Long,
+    val totalBudget: Long,
+    val usedBudget: Long,
 ) {
 
     companion object {
         fun from(dailyRoulette: DailyRoulette): RouletteRes =
             RouletteRes(
-                dailyRoulette.remainingBudget
+                dailyRoulette.totalBudget,
+                dailyRoulette.usedBudget
             )
     }
 }
