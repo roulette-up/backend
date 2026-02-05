e97a112c45add22e44a4ca82f6a85379ff0cda3b
DaeYoung0726 <pdy000726@naver.com>
2026-02-05 16:40:39 +0900

    feat: 사용자 생성 및 닉네임을 통한 조회 로직 추가

---
+++ rouletteup-domain/src/main/kotlin/kr/co/rouletteup/domain/user/service/UserService.kt
@@ -0,0 +1,18 @@
+package kr.co.rouletteup.domain.user.service
+
+import kr.co.rouletteup.domain.user.entity.User
+import kr.co.rouletteup.domain.user.repository.UserRepository
+import org.springframework.stereotype.Service
+
+@Service
+class UserService(
+    private val userRepository: UserRepository,
+) {
+
+    fun save(user: User): User =
+        userRepository.save(user)
+
+    fun readByNickname(nickname: String): User? =
+        userRepository.findByNickname(nickname)
+
+}
