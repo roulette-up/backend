c9959ca24f2201db322455e08e5d7934ae2bf335
DaeYoung0726 <pdy000726@naver.com>
2026-02-05 18:28:52 +0900

    feat: 룰렛 생성 및 존재 확인 로직 추가

---
+++ rouletteup-domain/src/main/kotlin/kr/co/rouletteup/domain/roulette/service/DailyRouletteService.kt
@@ -0,0 +1,19 @@
+package kr.co.rouletteup.domain.roulette.service
+
+import java.time.LocalDate
+import kr.co.rouletteup.domain.roulette.entity.DailyRoulette
+import kr.co.rouletteup.domain.roulette.repository.DailyRouletteRepository
+import org.springframework.stereotype.Service
+
+@Service
+class DailyRouletteService(
+    private val dailyRouletteRepository: DailyRouletteRepository,
+) {
+
+    fun save(dailyRoulette: DailyRoulette) {
+        dailyRouletteRepository.save(dailyRoulette)
+    }
+
+    fun existsToday(today: LocalDate): Boolean =
+        dailyRouletteRepository.existsByRouletteDate(today)
+}
