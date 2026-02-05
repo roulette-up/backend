15778c9040877dcd26f86535ac091bbe06854a4b
DaeYoung0726 <pdy000726@naver.com>
2026-02-05 10:47:00 +0900

    chore: JPA 및 DB 디펜던시 추가

--- rouletteup-domain/build.gradle.kts
+++ rouletteup-domain/build.gradle.kts
@@ -3,4 +3,10 @@ tasks.jar { enabled = true }
 
 dependencies {
     implementation(project(":rouletteup-common"))
+
+    /* JPA */
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    /* DB */
+    runtimeOnly("com.mysql:mysql-connector-j")
 }
