9489f08931639b69a238a01492e68cda38a002e4
DaeYoung0726 <pdy000726@naver.com>
2026-02-07 00:56:12 +0900

    feat: 사용자 예외 문구 추가

--- rouletteup-domain/src/main/kotlin/kr/co/rouletteup/domain/user/exception/UserErrorType.kt
+++ rouletteup-domain/src/main/kotlin/kr/co/rouletteup/domain/user/exception/UserErrorType.kt
@@ -3,10 +3,16 @@ package kr.co.rouletteup.domain.user.exception
 import kr.co.rouletteup.common.response.StatusCode
 import kr.co.rouletteup.common.response.error.type.BaseErrorType
 
+/**
+ * User ErrorCode: UXXX
+ */
 enum class UserErrorType(
     override val status: StatusCode,
     override val code: String,
     override val message: String,
 ) : BaseErrorType {
+
+    NOT_FOUND(StatusCode.NOT_FOUND, "U001", "사용자가 존재하지 않습니다."),
+    ;
 
 }
