fd1026e4c282dc606a85b57d1b407015b42ee82e
DaeYoung0726 <pdy000726@naver.com>
2026-02-06 23:18:40 +0900

    feat: 커서 기반 알림 페이징 조회 로직 추가

--- rouletteup-domain/src/main/kotlin/kr/co/rouletteup/domain/notification/service/NotificationService.kt
+++ rouletteup-domain/src/main/kotlin/kr/co/rouletteup/domain/notification/service/NotificationService.kt
@@ -2,14 +2,26 @@ package kr.co.rouletteup.domain.notification.service
 
 import kr.co.rouletteup.domain.notification.entity.Notification
 import kr.co.rouletteup.domain.notification.repository.NotificationRepository
+import org.springframework.data.domain.Pageable
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
 class NotificationService(
-    private val notificationRepository: NotificationRepository
+    private val notificationRepository: NotificationRepository,
 ) {
 
     fun saveAll(notifications: List<Notification>) {
         notificationRepository.saveAllInBatch(notifications)
     }
+
+    fun readById(id: Long): Notification? =
+        notificationRepository.findByIdOrNull(id)
+
+    fun readFirstPage(userId: Long, pageable: Pageable): List<Notification> =
+        notificationRepository.findFirstPage(userId, pageable)
+
+    fun readNextPage(userId: Long, cursorId: Long, pageable: Pageable): List<Notification> =
+        notificationRepository.findNextPage(userId, cursorId, pageable)
+
 }
