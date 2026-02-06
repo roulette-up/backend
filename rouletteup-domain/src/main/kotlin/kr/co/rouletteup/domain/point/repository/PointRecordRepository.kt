c1b891c923d2cfc6bc63b3e6d143e2594f03b2b9
DaeYoung0726 <pdy000726@naver.com>
2026-02-06 16:52:47 +0900

    feat: userId를 통한 포인트 내역 페이징 조회 로직 구현

--- rouletteup-domain/src/main/kotlin/kr/co/rouletteup/domain/point/repository/PointRecordRepository.kt
+++ rouletteup-domain/src/main/kotlin/kr/co/rouletteup/domain/point/repository/PointRecordRepository.kt
@@ -2,8 +2,11 @@ package kr.co.rouletteup.domain.point.repository
 
 import java.time.LocalDate
 import kr.co.rouletteup.domain.point.entity.PointRecord
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface PointRecordRepository : JpaRepository<PointRecord, Long> {
     fun existsByUserIdAndRouletteDate(userId: Long, rouletteDate: LocalDate): Boolean
+    fun findAllByUserId(userId: Long, pageable: Pageable): Page<PointRecord>
 }
