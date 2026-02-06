97830524cf386ad890eaff8a5387f286d69b0009
DaeYoung0726 <pdy000726@naver.com>
2026-02-07 01:53:51 +0900

    feat: 상품 조회 로직 추가

---
+++ rouletteup-domain/src/main/kotlin/kr/co/rouletteup/domain/product/service/ProductService.kt
@@ -0,0 +1,25 @@
+package kr.co.rouletteup.domain.product.service
+
+import kr.co.rouletteup.domain.product.entity.Product
+import kr.co.rouletteup.domain.product.repository.ProductRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+
+@Service
+class ProductService(
+    private val productRepository: ProductRepository,
+) {
+
+    fun save(product: Product) {
+        productRepository.save(product)
+    }
+
+    fun readById(id: Long): Product? =
+        productRepository.findByIdOrNull(id)
+
+    fun readAll(pageable: Pageable): Page<Product> =
+        productRepository.findAll(pageable)
+
+}
