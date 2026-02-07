6f7f03410520a1616f99616c09bb3f58b8dc5dcf
DaeYoung0726 <pdy000726@naver.com>
2026-02-07 21:59:15 +0900

    feat: 상품 업데이트 편의 메서드 추가

--- rouletteup-domain/src/main/kotlin/kr/co/rouletteup/domain/product/entity/Product.kt
+++ rouletteup-domain/src/main/kotlin/kr/co/rouletteup/domain/product/entity/Product.kt
@@ -4,6 +4,8 @@ import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Table
 import kr.co.rouletteup.domain.common.entity.BaseEntity
+import kr.co.rouletteup.domain.product.exception.ProductErrorType
+import kr.co.rouletteup.domain.product.exception.ProductException
 import org.hibernate.annotations.SQLDelete
 import org.hibernate.annotations.SQLRestriction
 
@@ -28,4 +30,25 @@ class Product(
     @Column(nullable = false)
     var price: Long = price
         protected set
+
+    /**
+     * 상품 정보 업데이트 편의 메서드
+     */
+    fun updateInfo(name: String, price: Long) {
+        this.name = name
+        this.price = price
+    }
+
+    /**
+     * 상품 재고 증감 처리 편의 메서드
+     */
+    fun changeStock(amount: Int) {
+        val newStock = this.stockQuantity + amount
+
+        if (newStock < 0) {
+            throw ProductException(ProductErrorType.INSUFFICIENT_STOCK)
+        }
+
+        this.stockQuantity = newStock
+    }
 }
