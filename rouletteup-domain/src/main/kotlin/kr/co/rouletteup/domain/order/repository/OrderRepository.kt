d53e09c42a088591ab9e9fdb35b4f944d57073be
DaeYoung0726 <pdy000726@naver.com>
2026-02-07 17:52:19 +0900

    feat: soft delete 포함 주문 내역 조회 로직 구현 (Admin)

--- rouletteup-domain/src/main/kotlin/kr/co/rouletteup/domain/order/repository/OrderRepository.kt
+++ rouletteup-domain/src/main/kotlin/kr/co/rouletteup/domain/order/repository/OrderRepository.kt
@@ -4,7 +4,49 @@ import kr.co.rouletteup.domain.order.entity.Order
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface OrderRepository : JpaRepository<Order, Long> {
     fun findAllByUserId(userId: Long, pageable: Pageable): Page<Order>
+
+    @Query(
+        value = """
+            SELECT *
+            FROM orders
+            WHERE user_id = :userId
+            ORDER BY id DESC
+        """,
+        countQuery = """
+            SELECT COUNT(*)
+            FROM orders
+            WHERE user_id = :userId
+        """, nativeQuery = true
+    )
+    fun findAllByUserIdIdIncludeDeleted(
+        @Param("userId") userId: Long,
+        pageable: Pageable
+    ): Page<Order>
+
+    @Query(
+        value = """
+            SELECT *
+            FROM orders
+            ORDER BY id DESC
+        """,
+        countQuery = """
+            SELECT COUNT(*)
+            FROM orders
+        """, nativeQuery = true
+    )
+    fun findAllIncludeDeleted(pageable: Pageable): Page<Order>
+
+    @Query(
+        value = """
+        SELECT *
+        FROM orders
+        WHERE id = :orderId
+    """, nativeQuery = true
+    )
+    fun findByIdIncludeDeleted(@Param("orderId") orderId: Long): Order?
 }
--- rouletteup-domain/src/main/kotlin/kr/co/rouletteup/domain/order/service/OrderService.kt
+++ rouletteup-domain/src/main/kotlin/kr/co/rouletteup/domain/order/service/OrderService.kt
@@ -21,4 +21,12 @@ class OrderService(
     fun readAllByUserId(userId: Long, pageable: Pageable): Page<Order> =
         orderRepository.findAllByUserId(userId, pageable)
 
+    fun readByIdIncludeDeleted(id: Long): Order? =
+        orderRepository.findByIdIncludeDeleted(id)
+
+    fun readAllByUserIdIncludeDeleted(userId: Long, pageable: Pageable): Page<Order> =
+        orderRepository.findAllByUserIdIdIncludeDeleted(userId, pageable)
+
+    fun readAllIncludeDeleted(pageable: Pageable): Page<Order> =
+        orderRepository.findAllIncludeDeleted(pageable)
 }
