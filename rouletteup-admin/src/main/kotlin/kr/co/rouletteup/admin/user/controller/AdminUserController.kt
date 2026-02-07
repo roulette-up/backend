de7631148b4f63a87393eee2d2b0a39688f41bb1
DaeYoung0726 <pdy000726@naver.com>
2026-02-07 17:51:55 +0900

    feat: 어드민 전체 사용자 및 특정 사용자 조회 로직 rest api 작성

---
+++ rouletteup-admin/src/main/kotlin/kr/co/rouletteup/admin/user/controller/AdminUserController.kt
@@ -0,0 +1,39 @@
+package kr.co.rouletteup.admin.user.controller
+
+import kr.co.rouletteup.admin.user.usecase.GetUserForAdminUseCase
+import kr.co.rouletteup.common.response.success.SuccessResponse
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.web.PageableDefault
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/admin/users")
+class AdminUserController(
+    private val getUserForAdminUseCase: GetUserForAdminUseCase,
+) {
+
+    @GetMapping
+    fun getUsers(
+        @PageableDefault(sort = ["id"], direction = Sort.Direction.DESC) pageable: Pageable,
+    ): ResponseEntity<*> =
+        ResponseEntity.ok(
+            SuccessResponse.from(
+                getUserForAdminUseCase.getUsers(pageable)
+            )
+        )
+
+    @GetMapping("/{userId}")
+    fun getUserById(
+        @PathVariable userId: Long
+    ): ResponseEntity<*> =
+        ResponseEntity.ok(
+            SuccessResponse.from(
+                getUserForAdminUseCase.getUserById(userId)
+            )
+        )
+}
