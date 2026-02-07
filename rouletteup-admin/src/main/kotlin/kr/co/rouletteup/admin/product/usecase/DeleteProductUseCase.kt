50ada1f15edcf0b3155c6c7edea52a8b213f3ca2
DaeYoung0726 <pdy000726@naver.com>
2026-02-07 21:59:33 +0900

    feat: 상품 삭제 로직 추가

---
+++ rouletteup-admin/src/main/kotlin/kr/co/rouletteup/admin/product/usecase/DeleteProductUseCase.kt
@@ -0,0 +1,22 @@
+package kr.co.rouletteup.admin.product.usecase
+
+import kr.co.rouletteup.domain.product.service.ProductService
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class DeleteProductUseCase(
+    private val productService: ProductService,
+) {
+
+    /**
+     * 상품 삭제 메서드
+     *
+     * @param productId 상품 ID(PK)
+     */
+    @Transactional
+    fun deleteProduct(productId: Long) {
+        productService.deleteById(productId)
+    }
+
+}
