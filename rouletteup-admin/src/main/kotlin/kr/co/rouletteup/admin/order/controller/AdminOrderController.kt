a814a6436212547c9898fe230b4392efda6890ac
DaeYoung0726 <pdy000726@naver.com>
2026-02-07 17:53:32 +0900

    feat: 어드민 전체 주문 내역 및 특정 주문 내역 조회 rest api 연결

---
+++ rouletteup-admin/src/main/kotlin/kr/co/rouletteup/admin/order/controller/AdminOrderController.kt
@@ -0,0 +1,41 @@
+package kr.co.rouletteup.admin.order.controller
+
+import kr.co.rouletteup.admin.order.usecase.GetOrderForAdminUseCase
+import kr.co.rouletteup.common.response.success.SuccessResponse
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.web.PageableDefault
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/admin/orders")
+class AdminOrderController(
+    private val getOrderForAdminUseCase: GetOrderForAdminUseCase,
+) {
+
+    @GetMapping
+    fun getOrders(
+        @RequestParam(required = false) userId: Long?,
+        @PageableDefault(sort = ["id"], direction = Sort.Direction.DESC) pageable: Pageable,
+    ): ResponseEntity<*> =
+        ResponseEntity.ok(
+            SuccessResponse.from(
+                getOrderForAdminUseCase.getOrders(userId, pageable)
+            )
+        )
+
+    @GetMapping("/{orderId}")
+    fun getOrderById(
+        @PathVariable orderId: Long
+    ): ResponseEntity<*> =
+        ResponseEntity.ok(
+            SuccessResponse.from(
+                getOrderForAdminUseCase.getOrderById(orderId)
+            )
+        )
+}
