9fb99ae34f9d180e6bbc7bc2545418d175317d35
DaeYoung0726 <pdy000726@naver.com>
2026-02-07 23:01:39 +0900

    feat: 어드민이 사용자 주문 내역을 취소하는 로직 rest api 작성

--- rouletteup-admin/src/main/kotlin/kr/co/rouletteup/admin/order/controller/AdminOrderController.kt
+++ rouletteup-admin/src/main/kotlin/kr/co/rouletteup/admin/order/controller/AdminOrderController.kt
@@ -1,6 +1,7 @@
 package kr.co.rouletteup.admin.order.controller
 
 import kr.co.rouletteup.admin.order.api.AdminOrderApi
+import kr.co.rouletteup.admin.order.usecase.CancelUserOrderForAdminUseCase
 import kr.co.rouletteup.admin.order.usecase.GetOrderForAdminUseCase
 import kr.co.rouletteup.common.response.success.SuccessResponse
 import org.springframework.data.domain.Pageable
@@ -8,6 +9,7 @@ import org.springframework.data.domain.Sort
 import org.springframework.data.web.PageableDefault
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/v1/admin")
 class AdminOrderController(
     private val getOrderForAdminUseCase: GetOrderForAdminUseCase,
+    private val cancelUserOrderForAdminUseCase: CancelUserOrderForAdminUseCase,
 ) : AdminOrderApi {
 
     @GetMapping("/users/{userId}/orders")
@@ -49,4 +52,13 @@ class AdminOrderController(
                 getOrderForAdminUseCase.getOrderById(orderId)
             )
         )
+
+    @PatchMapping("/orders/{orderId}/cancel")
+    fun cancelUserOrder(
+        @PathVariable orderId: Long,
+    ): ResponseEntity<*> {
+        cancelUserOrderForAdminUseCase.cancelUserOrder(orderId)
+        return ResponseEntity.ok(SuccessResponse.ok())
+    }
+
 }
