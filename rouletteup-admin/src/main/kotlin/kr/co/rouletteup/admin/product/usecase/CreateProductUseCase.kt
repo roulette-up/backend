e92a13b74cbaf47f32f46cca36379b29a784261a
DaeYoung0726 <pdy000726@naver.com>
2026-02-07 21:58:01 +0900

    feat: 상품 생성 로직 추가

---
+++ rouletteup-admin/src/main/kotlin/kr/co/rouletteup/admin/product/usecase/CreateProductUseCase.kt
@@ -0,0 +1,23 @@
+package kr.co.rouletteup.admin.product.usecase
+
+import kr.co.rouletteup.admin.product.dto.AdminProductReq
+import kr.co.rouletteup.domain.product.service.ProductService
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class CreateProductUseCase(
+    private val productService: ProductService,
+) {
+
+    /**
+     * 상품 생성 메서드
+     *
+     * @param request 상품 생성 요청 DTO
+     */
+    @Transactional
+    fun createProduct(request: AdminProductReq.Create) {
+        productService.save(request.toEntity())
+    }
+
+}
