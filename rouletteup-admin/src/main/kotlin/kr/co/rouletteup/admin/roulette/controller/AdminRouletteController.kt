3580622e0880c710f2add39b2b2334f1d01018ab
DaeYoung0726 <pdy000726@naver.com>
2026-02-07 19:01:55 +0900

    feat: 어드민 전체 룰렛 조회 로직 rest api 작성

---
+++ rouletteup-admin/src/main/kotlin/kr/co/rouletteup/admin/roulette/controller/AdminRouletteController.kt
@@ -0,0 +1,28 @@
+package kr.co.rouletteup.admin.roulette.controller
+
+import kr.co.rouletteup.admin.roulette.usecase.GetRouletteForAdminUseCase
+import kr.co.rouletteup.common.response.success.SuccessResponse
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.web.PageableDefault
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/admin/roulettes")
+class AdminRouletteController(
+    private val getRouletteForAdminUseCase: GetRouletteForAdminUseCase
+) {
+
+    @GetMapping
+    fun getRoulettes(
+        @PageableDefault(sort = ["id"], direction = Sort.Direction.DESC) pageable: Pageable,
+    ): ResponseEntity<*> =
+        ResponseEntity.ok(
+            SuccessResponse.from(
+                getRouletteForAdminUseCase.getRoulettes(pageable)
+            )
+        )
+}
