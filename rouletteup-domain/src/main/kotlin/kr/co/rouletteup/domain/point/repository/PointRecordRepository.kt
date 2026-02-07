61e7b8df8eb94fcf3a3121de7355e2a71087ae86
DaeYoung0726 <pdy000726@naver.com>
2026-02-07 11:21:58 +0900

    feat: 사용자 사용 가능한 포인트 전체 조회 로직 추가

--- rouletteup-domain/src/main/kotlin/kr/co/rouletteup/domain/point/repository/PointRecordRepository.kt
+++ rouletteup-domain/src/main/kotlin/kr/co/rouletteup/domain/point/repository/PointRecordRepository.kt
@@ -2,6 +2,7 @@ package kr.co.rouletteup.domain.point.repository
 
 import java.time.LocalDate
 import kr.co.rouletteup.domain.point.entity.PointRecord
+import kr.co.rouletteup.domain.point.type.PointStatus
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
@@ -12,6 +13,7 @@ import org.springframework.data.repository.query.Param
 interface PointRecordRepository : JpaRepository<PointRecord, Long> {
     fun existsByUserIdAndRouletteDate(userId: Long, rouletteDate: LocalDate): Boolean
     fun findAllByUserId(userId: Long, pageable: Pageable): Page<PointRecord>
+    fun findAllByUserIdAndStatusOrderByExpiresAtAsc(userId: Long, status: PointStatus): List<PointRecord>
 
     @Query(
         value = """
