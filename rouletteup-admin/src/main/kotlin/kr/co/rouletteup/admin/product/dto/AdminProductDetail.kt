5d75a48cf73e9e42e48c7be3673c76f5432a24a4
DaeYoung0726 <pdy000726@naver.com>
2026-02-07 21:58:32 +0900

    feat: 상품 생성 조회 로직 추가

---
+++ rouletteup-admin/src/main/kotlin/kr/co/rouletteup/admin/product/dto/AdminProductDetail.kt
@@ -0,0 +1,26 @@
+package kr.co.rouletteup.admin.product.dto
+
+import java.time.LocalDateTime
+import kr.co.rouletteup.domain.product.entity.Product
+
+data class AdminProductDetail(
+    val id: Long,
+    val name: String,
+    val stockQuantity: Int,
+    val price: Long,
+    val createdAt: LocalDateTime,
+    val deletedAt: LocalDateTime?,
+) {
+
+    companion object {
+        fun from(product: Product): AdminProductDetail =
+            AdminProductDetail(
+                id = product.id!!,
+                name = product.name,
+                stockQuantity = product.stockQuantity,
+                price = product.price,
+                createdAt = product.createdAt,
+                deletedAt = product.deletedAt
+            )
+    }
+}
---
+++ rouletteup-admin/src/main/kotlin/kr/co/rouletteup/admin/product/usecase/GetProductUseCase.kt
@@ -0,0 +1,42 @@
+package kr.co.rouletteup.admin.product.usecase
+
+import kr.co.rouletteup.admin.product.dto.AdminProductDetail
+import kr.co.rouletteup.admin.product.dto.AdminProductSummary
+import kr.co.rouletteup.domain.product.exception.ProductErrorType
+import kr.co.rouletteup.domain.product.exception.ProductException
+import kr.co.rouletteup.domain.product.service.ProductService
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class GetProductUseCase(
+    private val productService: ProductService,
+) {
+
+    /**
+     * 전체 상품 조회 메서드 (soft delete 포함)
+     *
+     * @param pageable 페이지 크기
+     * @return 상품 페이징 DTO
+     */
+    @Transactional(readOnly = true)
+    fun getProducts(pageable: Pageable): Page<AdminProductSummary> =
+        productService.readAllIncludeDeleted(pageable)
+            .map { product -> AdminProductSummary.from(product) }
+
+    /**
+     * 특정 상품 조회 메서드 (soft delete 포함)
+     *
+     * @param productId 상품 ID(PK)
+     * @return 상품 정보 DTO
+     */
+    @Transactional(readOnly = true)
+    fun getProductById(productId: Long): AdminProductDetail {
+        val product = productService.readByIdIncludeDeleted(productId)
+            ?: throw ProductException(ProductErrorType.NOT_FOUND)
+
+        return AdminProductDetail.from(product)
+    }
+}
