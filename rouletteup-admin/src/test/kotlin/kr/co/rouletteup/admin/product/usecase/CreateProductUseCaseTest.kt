99e1cfda50a1b71bff932b063477a89dc0a25648
DaeYoung0726 <pdy000726@naver.com>
2026-02-07 21:58:16 +0900

    test: 상품 생성 로직 test code 작성

---
+++ rouletteup-admin/src/test/kotlin/kr/co/rouletteup/admin/product/usecase/CreateProductUseCaseTest.kt
@@ -0,0 +1,49 @@
+package kr.co.rouletteup.admin.product.usecase
+
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.mockk
+import io.mockk.verify
+import kr.co.rouletteup.admin.product.dto.AdminProductReq
+import kr.co.rouletteup.domain.product.entity.Product
+import kr.co.rouletteup.domain.product.service.ProductService
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+
+@ExtendWith(MockKExtension::class)
+class CreateProductUseCaseTest {
+
+    @MockK
+    private lateinit var productService: ProductService
+
+    @InjectMockKs
+    private lateinit var createProductUseCase: CreateProductUseCase
+
+    @Nested
+    @DisplayName("상품 생성")
+    inner class CreateProduct {
+
+        @Test
+        fun `상품 생성 요청이면 Product로 변환 후 저장한다`() {
+            // given
+            val request = mockk<AdminProductReq.Create>()
+            val product = mockk<Product>(relaxed = true)
+
+            every { request.toEntity() } returns product
+            every { productService.save(product) } returns Unit
+
+            // when
+            createProductUseCase.createProduct(request)
+
+            // then
+            verify(exactly = 1) { request.toEntity() }
+            verify(exactly = 1) { productService.save(product) }
+        }
+    }
+
+}
