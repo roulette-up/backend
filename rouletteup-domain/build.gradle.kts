618d66bf09abcf438b49bd83111047c23e8b9493
DaeYoung0726 <pdy000726@naver.com>
2026-02-10 02:09:18 +0900

    chore: QueryDsl 디펜던시 추가

--- rouletteup-domain/build.gradle.kts
+++ rouletteup-domain/build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    kotlin("kapt")
+}
+
 tasks.bootJar { enabled = false }
 tasks.jar { enabled = true }
 
@@ -9,4 +13,16 @@ dependencies {
 
     /* DB */
     runtimeOnly("com.mysql:mysql-connector-j")
+
+    // QueryDSL
+    implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+    kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
+    kapt("jakarta.annotation:jakarta.annotation-api")
+    kapt("jakarta.persistence:jakarta.persistence-api")
+}
+
+kotlin {
+    sourceSets.main {
+        kotlin.srcDir("build/generated/source/kapt/main")
+    }
 }
